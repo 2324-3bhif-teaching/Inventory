e72fa6ba5249b4e89e575cc6d5b7af4c06be0b3b
Benjamin Wilflingseder <b.wilflingseder@students.htl-leonding.ac.at>
2024-06-13 22:15:29 +0200

    updated app.ts with keycloak stuff, sadly not working yet

--- InventoryCode/src/app.ts
+++ InventoryCode/src/app.ts
@@ -10,8 +10,29 @@ import {itemRouter} from "./Router/ItemRouter";
 import categoryRouter from './Router/categoryRouter';
 import {uploadRouter} from "./Router/uploadRouter";
 import path from "node:path";
+import session from 'express-session';
+import Keycloak from 'keycloak-connect';
 // create express application
 export const app = express();
+
+const memoryStore = new session.MemoryStore();
+app.use(session({
+    secret: 'u4ZhRf6B@@FUmLsFmpeGSdQKmfZ@YVBYqc@zQh9Re2y3^VzLV5rST$9EbPW2&%X!9dLz5piKHBmjcMPU4hqZzm3ud6Y7h*aMzNA^^@BDn2!BC7a', //TODO: change secret and save in .env
+    resave: false,
+    saveUninitialized: true,
+    store: memoryStore
+}));
+const keycloak = new Keycloak({ store: memoryStore });
+
+app.use(keycloak.middleware({
+    logout: '/logout'
+}));
+
+app.get('/logout', keycloak.protect(), (req: any, res) => {
+    req.kauth.logout();
+    res.redirect('http://localhost:3000/index.html');
+});
+
 
 // mount middleware
 app.use(cors());
